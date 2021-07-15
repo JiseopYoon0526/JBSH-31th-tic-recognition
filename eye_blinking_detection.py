5c10ef3a87a0e120511e0a240e5eb5fadd454224
JiseopYoon0526 <76778406+JiseopYoon0526@users.noreply.github.com>
2021-07-15 20:50:11 +0900

    Update eye_blinking_detection.py

--- eye_blinking_detection.py
+++ eye_blinking_detection.py
@@ -1,21 +1,26 @@
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.models import Sequential
+from keras.utils import np_utils
 import tensorflow as tf
+import numpy as np
+from tensorflow import keras
 
 # Loading Dataset
 
-x_train = np.load('dataset/x_train.npy').astype(np.float32)
-y_train = np.load('dataset/y_train.npy').astype(np.float32)
-x_val = np.load('dataset/x_val.npy').astype(np.float32)
-y_val = np.load('dataset/y_val.npy').astype(np.float32)
+x_train = np.load('x_train.npy').astype(np.float32)
+y_train = np.load('y_train.npy').astype(np.float32)
+x_val = np.load('x_val.npy').astype(np.float32)
+y_val = np.load('y_val.npy').astype(np.float32)
 
-model = model.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28,1)))
-model.add(layers.MaxPooling2D((2,2))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model = Sequential()
+model.add(keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28,1)))
+model.add(keras.layers.MaxPooling2D((2,2)))
+model.add(keras.layers.Conv2D(64, (3,3), activation='relu'))
+model.add(keras.layers.MaxPooling2D((2,2)))
+model.add(keras.layers.Conv2D(64, (3,3), activation='relu'))
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(320, activation='relu'))
+model.add(keras.layers.Dense(100, activation='softmax'))
 model.summary()
+
